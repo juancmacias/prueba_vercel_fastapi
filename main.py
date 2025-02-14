ce084c83e73e2c6043d4d516a90e98d4af8e6896
juan carlos macias <juancmaciassalvador@gmail.com>
2025-02-14 15:01:26 +0100

    mas

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-#from fastapi.templating import Jinja2Templates
+from fastapi.templating import Jinja2Templates
 # instalar fastapi uvicorn
 # para correr el proyecto uvicorn main:app --reload
 app = FastAPI()
 variable_precio = 1000
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-#templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="templates")
 
-
+@app.get("/r", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"id": variable_precio}
+    )
 
 @app.get("/")
 async def read_root():
